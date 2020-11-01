54c19dfcf510a81b7e800e30664a7794d2aaa07e
tuguldur <aa402115@gmail.com>
2020-11-01 21:44:03 +0800

    pagination

--- list.php
+++ list.php
@@ -1,3 +1,16 @@
+<?php 
+
+   include './lib/config.php';
+   $limit = 5;
+   $page = isset($_GET['page']) ? $_GET['page'] : 1;
+   $start = ($page - 1) * $limit;
+   $result = $connect->query("SELECT * FROM students LIMIT $start, $limit");
+
+   $count = $connect->query("SELECT count(id) AS id FROM students");
+   $pages = ceil( $count->fetch_assoc()['id'] / $limit );
+   $previous = $page - 1;
+   $next = $page + 1;
+?>
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -49,9 +62,6 @@
   </thead>
   <tbody>
    <?php
-include './lib/config.php';
-   
-$result = mysqli_query($connect,"SELECT * FROM students");
 
 while($row = mysqli_fetch_array($result))
 {
@@ -77,6 +87,25 @@ $num_rows = mysqli_num_rows($result);
 $connect->close();
 echo "Нийт <b>$num_rows</b> Оюутан\n";
 ?>
+<div class="mt-2">
+	   <nav aria-label="Page navigation">
+			<ul class="pagination justify-content-end">
+				<li class="<?= $previous == 0 ? 'disabled': '' ?> page-item">
+			      <a class="page-link" href="list.php?page=<?= $previous ?>" aria-label="Previous">
+				     <span aria-hidden="true">&laquo;</span>
+				   </a>
+				</li>
+				<?php for($i = 1; $i<= $pages; $i++) : ?>
+				<li  class="page-item <?= $page == $i ?'active' :'' ?>"><a class="page-link" href="list.php?page=<?= $i; ?>"><?= $i; ?></a></li>
+				<?php endfor; ?>
+				<li class="<?= $result->num_rows < $limit ? 'disabled': '' ?> page-item">
+				   <a class="page-link" href="list.php?page=<?= $next; ?>" aria-label="Next">
+				      <span aria-hidden="true">&raquo;</span>
+				   </a>
+				</li>
+			</ul>
+		</nav>
+	</div>
 </div>
       <script src="assets/js/jquery-3.5.1.min.js"></script>
       <script src="assets/js/popper.min.js"></script>
